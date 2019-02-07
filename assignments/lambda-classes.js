ca3a561a8ea5997176a7dea9f51561a495790c19
jackgipson <jackjgipson@gmail.com>
2019-02-07 14:55:33 -0800

    added methods to Instructor classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,7 +9,7 @@ class Person {
     }
     
     speak() {
-        `Hello my name is ${ this.name}, I am from ${this.location}`;
+        return `Hello my name is ${ this.name}, I am from ${this.location}`;
     }
 }
 
@@ -20,9 +20,14 @@ class Instructor extends Person {
         this.favLanguage = instructorAtts.favLanguage;
         this.catchPhrase = instructorAtts.catchPhrase;
     }
+    demo(subject) { //demo receives a subject string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+        return `Today we are learning about ${subject}`;
+      }
 
-
-}
+    grade(student, subject) { //grade receives a student object and a subject string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+        return `${student.name} receives a perfect score on ${subject}`;
+      }
+    }
 
 class Student extends Person {
     constructor(studentAtts) {
@@ -30,7 +35,12 @@ class Student extends Person {
         this. previousBackground = studentAtts.previousBackground;
         this.className = studentAtts.className;
         this.favSubjects = studentAtts.favSubjects;
+        
+    }
+    listSubjects() {
+        const listsSubjects = ['Math', 'Science', "Redux"];
 
+    listsSubjects.forEach( favoriteSubjects => console.log(favoriteSubjects));
     }
 }
 
@@ -72,3 +82,7 @@ console.log(jess.previousBackground);
 console.log(jess.className);
 console.log(shawn.favInstructor);
 console.log(shawn.gradClassName);
+
+console.log(dan.speak());
+console.log(shawn.demo('Math'));
+console.log(dan.grade(dan, 'JavaScript'));
