bb20e8b325d7d754ae66df6dbc4691d847b2fa36
jackgipson <jackjgipson@gmail.com>
2019-02-07 12:26:29 -0800

    finished the basic set up need to add methods in classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -42,3 +42,33 @@ class ProjectManager extends Instructor {
     }
 }
 
+
+const dan = new Instructor({
+    age: 29,
+    name: "Dan",
+    location: "Utah",
+    gender: "M",
+    specialty: "React",
+    favLanguage: ["Javascript", "Python", "Elm"],
+    catchphrase: "Don't forget the homies"
+  });
+
+const jess = new Student({
+    previousBackground: "Construction",
+    className: "CS132",
+    favSubjects: ['Html', 'CSS', 'JavaScript']
+});
+
+const shawn = new ProjectManager({
+    gradClassName: "CS1",
+    favInstructor: "Dan"
+})
+
+
+console.log(dan.age);
+console.log(dan.location);
+console.log(dan.favLanguage);
+console.log(jess.previousBackground);
+console.log(jess.className);
+console.log(shawn.favInstructor);
+console.log(shawn.gradClassName);
